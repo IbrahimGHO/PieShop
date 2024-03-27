f19e62d90c9383f540f8c0e5624b0ce6c9172cef
Ibra <ibygho@gmail.com>
2024-03-27 14:56:43 +0300

    Routing, Nav Bar & Home Page

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+
+        }
+        public IActionResult Index()
+        {
+            var PiesOfTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(PiesOfTheWeek);
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ WebApplication2/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get;}
+
+   
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek= piesOfTheWeek;
+        }
+    }
+}
